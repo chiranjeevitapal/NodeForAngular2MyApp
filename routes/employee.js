868144642c8eaa49be6c3fff963db7ae711590bc
Chiranjeevi Tapal <ctapal@qti.qualcomm.com>
2016-12-12 16:12:06 +0530

    added code for file upload

--- routes/employee.js
+++ routes/employee.js
@@ -2,132 +2,151 @@ var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
 var bcrypt = require('bcryptjs');
-var db = mongojs('mongodb://localhost:27017/jobu', ['employees']);
+var fs = require('fs');
+var multer = require('multer');
+var db = mongojs('mongodb://localhost:27017/jobu', [ 'employees' ]);
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var mkdirp = require('mkdirp');
 
 /* Get all users */
 router.get('/employees', function(req, res, next) {
-  var collection = db.collection('employees');
-  collection.find({}).toArray(function(err, employees) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(employees);
-    }
-  });
+	var collection = db.collection('employees');
+	collection.find({}).toArray(function(err, employees) {
+		if (err) {
+			res.send(err);
+		} else {
+			res.json(employees);
+		}
+	});
 });
 /* GET One Todo with the provided ID */
 router.get('/employeeDetail/:userName', function(req, res, next) {
-  var userName = req.params.userName;
-  db.employees.findOne({
-    // _id : mongojs.ObjectId(req.params.email)
-    _id: userName
-  }, function(err, employee) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(employee);
-    }
-  });
+	var userName = req.params.userName;
+	db.employees.findOne({
+		// _id : mongojs.ObjectId(req.params.email)
+		_id : userName
+	}, function(err, employee) {
+		if (err) {
+			res.send(err);
+		} else {
+			if (null != employee) {
+				res.json(employee);
+			} else {
+				return res.json({
+					code : 500,
+					error : "no employee found"
+				});
+			}
+		}
+	});
 });
 /* register a new employee */
 router.post('/registerEmployee', function(req, res, next) {
-  var employee = req.body.employee;
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(employee.password, salt, function(err, hash) {
-      employee.password = hash;
-      db.collection('employees').insert(employee, function(error, record) {
-        if (error) {
-          res.json(error);
-        } else {
-          res.json(record);
-        }
-      });
-    });
-  });
+	var employee = req.body.employee;
+	bcrypt.genSalt(10, function(err, salt) {
+		bcrypt.hash(employee.password, salt, function(err, hash) {
+			employee.password = hash;
+			db.collection('employees').insert(employee,
+					function(error, record) {
+						if (error) {
+							res.json(error);
+						} else {
+							res.json(record);
+						}
+					});
+		});
+	});
 
 });
 
 // Login
 router.get('/loginEmployee', function(req, res) {
-  res.render('loginEmployee');
+	res.render('loginEmployee');
 });
 
 passport.use(new LocalStrategy({
-  usernameField: 'userName',
-  passwordField: 'password',
-  passReqToCallback: true,
-  session: true
+	usernameField : 'userName',
+	passwordField : 'password',
+	passReqToCallback : true,
+	session : true
 }, function(req, username, password, done) {
-  getUserByUsername(username, function(err, user) {
-    if (err) throw err;
-    if (!user) { return done(null, false, {
-      message: 'Unknown employee'
-    }); }
+	getUserByUsername(username, function(err, user) {
+		if (err)
+			throw err;
+		if (!user) {
+			return done(null, false, {
+				message : 'Unknown employee'
+			});
+		}
 
-    comparePassword(password, user.password, function(err, isMatch) {
-      if (err) throw err;
-      if (isMatch) {
-        return done(null, user);
-      } else {
-        return done(null, false, {
-          message: 'Invalid password'
-        });
-      }
-    });
-  });
+		comparePassword(password, user.password, function(err, isMatch) {
+			if (err)
+				throw err;
+			if (isMatch) {
+				return done(null, user);
+			} else {
+				return done(null, false, {
+					message : 'Invalid password'
+				});
+			}
+		});
+	});
 }));
 
-//used to serialize the user for the session
+// used to serialize the user for the session
 passport.serializeUser(function(user, done) {
-	console.log("user.userName::::::  "+user.userName)
-    done(null, user.userName); 
-   // where is this user.id going? Are we supposed to access this anywhere?
+	done(null, user.userName);
+	// where is this user.id going? Are we supposed to access this anywhere?
 });
 
 // used to deserialize the user
 passport.deserializeUser(function(userName, done) {
 	db.collection('employees').findById(userName, function(err, user) {
-        done(err, userName);
-    });
+		done(err, userName);
+	});
 });
 
 getUserByUsername = function(username, callback) {
-  var query = {
-    userName: username
-  };
-  db.collection('employees').findOne(query, callback);
+	var query = {
+		userName : username
+	};
+	db.collection('employees').findOne(query, callback);
 }
 
 comparePassword = function(candidatePassword, hash, callback) {
-  bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-    if (err) throw err;
-    callback(null, isMatch);
-  });
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+		if (err)
+			throw err;
+		callback(null, isMatch);
+	});
 }
 
 router.post('/loginEmployee', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) { return res.json({
-      code: 500,
-      error: info.message
-    }); }
-    if (!user) { return res.json({
-      code: 500,
-      error: info.message
-    }); }
-    req.logIn(user, function(err) {
-      if (err) {
-        return res.json({
-          code: 500,
-          error: info.message
-        });
-      } else {
-    	  return res.json(user);
-      }
-    });
-  })(req, res, next);
+	passport.authenticate('local', function(err, user, info) {
+		if (err) {
+			return res.json({
+				code : 500,
+				error : info.message
+			});
+		}
+		if (!user) {
+			return res.json({
+				code : 500,
+				error : info.message
+			});
+		}
+		req.logIn(user, function(err) {
+			if (err) {
+				return res.json({
+					code : 500,
+					error : info.message
+				});
+			} else {
+				return res.json(user);
+			}
+		});
+	})(req, res, next);
 });
 
 /* login with employee creds */
@@ -145,32 +164,32 @@ router.post('/loginEmployee', function(req, res, next) {
 
 /* PUT/UPDATE a user */
 router.put('/updateEmployee', function(req, res, next) {
-  var employee = req.body.employee;
-  var updObj = {};
-  if (employee.isCompleted) {
-    updObj.isCompleted = employee.isCompleted;
-  }
-  if (employee.text) {
-    updObj.text = employee.text;
-  }
-  if (!updObj) {
-    res.status(400);
-    res.json({
-      "error": "Invalid Data"
-    });
-  } else {
-    updObj = employee;
-    delete updObj["_id"];
-    db.employees.update({
-      _id: employee.userName
-    }, updObj, {}, function(err, record) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.json(employee);
-      }
-    });
-  }
+	var employee = req.body.employee;
+	var updObj = {};
+	if (employee.isCompleted) {
+		updObj.isCompleted = employee.isCompleted;
+	}
+	if (employee.text) {
+		updObj.text = employee.text;
+	}
+	if (!updObj) {
+		res.status(400);
+		res.json({
+			"error" : "Invalid Data"
+		});
+	} else {
+		updObj = employee;
+		delete updObj["_id"];
+		db.employees.update({
+			_id : employee.userName
+		}, updObj, {}, function(err, record) {
+			if (err) {
+				res.send(err);
+			} else {
+				res.json(employee);
+			}
+		});
+	}
 });
 
 /* DELETE a Todo */
@@ -180,5 +199,28 @@ router.put('/updateEmployee', function(req, res, next) {
  * db.users.remove({ id : Number(user.id) }, '', function(err, result) { if
  * (err) { res.send(err); } else { return res.json(user); } }); });
  */
+var storage = multer.diskStorage({
+	destination : function(req, file, callback) {
+		var dest = 'C:/Temp/';
+		mkdirp.sync(dest);
+		callback(null, dest);
+	},
+	filename : function(req, file, callback) {
+		callback(null, file.fieldname + '-' + Date.now()+'.docx');
+	}
+});
+
+var upload = multer({
+	storage : storage
+}).single('resume');
+
+router.post('/upload', function(req, res) {
+	upload(req, res, function(err) {
+		if (err) {
+			return res.end("Error uploading file.");
+		}
+		res.end("File is uploaded.");
+	});
+});
 
 module.exports = router;
