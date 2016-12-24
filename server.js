f26b3b677b0ba7fa272edf696cd571984dc463de
Ubuntu <ubuntu@ip-172-31-36-11.us-west-2.compute.internal>
2016-12-24 06:55:46 +0000

    changed rigin

--- server.js
+++ server.js
@@ -32,12 +32,11 @@ var app = express();
 
 app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin',
-			'http://localhost:3000');
+			'*');
 	res.setHeader('Access-Control-Allow-Methods', 'POST');
 	res.setHeader('Access-Control-Allow-Headers',
 			'X-Requested-With,content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.setHeader('access-control-expose-headers', 'X-Total-Count');
 	next();
 });
 
