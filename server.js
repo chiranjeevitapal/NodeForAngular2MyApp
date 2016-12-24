43f3af6610c159efc2947d28f3f1e5b6ee40398d
Ubuntu <ubuntu@ip-172-31-36-11.us-west-2.compute.internal>
2016-12-24 14:52:04 +0000

    changed port to 8090

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.use('/api/', uploader);
 app.use('/api/', walkins);
 
 // Set Port
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 8090));
 
 app.listen(app.get('port'), function() {
 	console.log('Server started on port ' + app.get('port'));
