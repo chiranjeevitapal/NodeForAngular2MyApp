8584bfc8d16119a2666e39fa0031662d74d1c641
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2017-02-23 18:46:20 +0530

    compressed data

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
+var expressStaticGzip = require("express-static-gzip");
+var compression = require('compression');
 
 mongoose.connect('mongodb://localhost:27017/jobu');
 var db = mongoose.connection;
@@ -31,53 +33,62 @@ var app = express();
  * app.use(cors(corsOptions))
  */
 
+ app.use(compression());
+ app.use("/", expressStaticGzip("client"));
 
-
-app.use(express.static(path.join(__dirname, 'client')));
+//app.use(express.static(path.join(__dirname, 'client')));
 //URL Rewriting - start
 
 
 app.get('/home', function(request, response, next) {
-    app.use(express.static(path.join(__dirname, 'client')))
+    //app.use(express.static(path.join(__dirname, 'client')))
+    app.use("/", expressStaticGzip("client"));
     request.url = '/#/home';
     next();
 });
 
 app.get('/uploadChethan', function(request, response, next) {
-    app.use(express.static(path.join(__dirname, 'client')))
+    //app.use(express.static(path.join(__dirname, 'client')))
+    app.use("/", expressStaticGzip("client"));
     request.url = '/#/uploadChethan';
     next();
 });
 app.get('/postJob', function(request, response, next) {
-    app.use(express.static(path.join(__dirname, 'client')))
+    //app.use(express.static(path.join(__dirname, 'client')))
+    app.use("/", expressStaticGzip("client"));
     request.url = '/#/postJob';
     next();
 });
 app.get('/jobseekers', function(request, response, next) {
-    app.use(express.static(path.join(__dirname, 'client')))
+    //app.use(express.static(path.join(__dirname, 'client')))
+    app.use("/", expressStaticGzip("client"));
     request.url = '/#/jobseekers';
     next();
 });
 app.get('/profile', function(request, response, next) {
-    app.use(express.static(path.join(__dirname, 'client')))
+    //app.use(express.static(path.join(__dirname, 'client')))
+    app.use("/", expressStaticGzip("client"));
     request.url = '/#/profile';
     next();
 });
 app.get('/tutorials', function(request, response, next) {
-    app.use(express.static(path.join(__dirname, 'client')))
+    //app.use(express.static(path.join(__dirname, 'client')))
+    app.use("/", expressStaticGzip("client"));
     request.url = '/#/tutorials';
     next();
 });
 
 app.get('/walkin/:id', function(request, response, next) {
-    app.use(express.static(path.join(__dirname, 'client')))
+    //app.use(express.static(path.join(__dirname, 'client')))
+    app.use("/", expressStaticGzip("client"));
     request.url = '/#/walkin/:id';
     next();
 });
 
 app.get('/**', function(request, response, next) {
     if (request.url.indexOf("/api/") == -1) {
-        app.use(express.static(path.join(__dirname, 'client')))
+        //app.use(express.static(path.join(__dirname, 'client')))
+        app.use("/", expressStaticGzip("client"));
         request.url = '/#/home';
     }
     next();
